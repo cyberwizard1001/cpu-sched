fd5b06d116d8e9b4674e678987aaf9ad82726480
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-03-31 02:52:57 +0530

    Corrections - 4 queues but hey it works

--- Driver.java
+++ Driver.java
@@ -9,7 +9,7 @@ public class Driver{
     //Main function for multi level feedback queue
     public static void main(String[] args){
         MultiLevelFeedbackQueue mfq=new MultiLevelFeedbackQueue();
-        mfq.retrieveProcess();
+        mfq.retrieveJobs();
         mfq.outputHeader();
         mfq.simulate();
         mfq.outputStats();
--- MultiLevelFeedbackQueue.java
+++ MultiLevelFeedbackQueue.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
+
 /**
  * Class to simulate a multi level feedback queue in which Process are scheduled for cpu processing
  * Modified: Nirmal Karthikeyan
@@ -8,17 +9,17 @@ import java.util.Scanner;
 
 
 public class MultiLevelFeedbackQueue {
-    static final int QUANTUM1 = 2, QUANTUM2 = 4, QUANTUM3 = 8, QUANTUM4 = 16, QUANTUM5 = 32, QUANTUM6 = 64, QUANTUM7 = 128, QUANTUM8 = 256;
-    int clock, numberOfProcess, sumTotalTime, sumResponseTime, totalSize;
+    static final int QUANTUM1 = 2, QUANTUM2 = 4, QUANTUM3 = 8, QUANTUM4 = 16;
+    int clock, numberOfJobs, sumTotalTime, sumResponseTime, totalSize;
     CentralProcessingUnit cpu;
-    ObjectQueue entrance, q1, q2, q3, q4,q5,q6,q7,q8;
-
+    ObjectQueue entrance, q1, q2, q3, q4;
 
     //Constructor for multi level feedback queue
     public MultiLevelFeedbackQueue() {
         clock = 0;
-        numberOfProcess = 0;
+        numberOfJobs = 0;
         sumTotalTime = 0;
+        sumResponseTime = 0;
         totalSize = 0;
         //declaring cpu object
         cpu = new CentralProcessingUnit();
@@ -29,16 +30,12 @@ public class MultiLevelFeedbackQueue {
         q2 = new ObjectQueue();
         q3 = new ObjectQueue();
         q4 = new ObjectQueue();
-        q5 = new ObjectQueue();
-        q6 = new ObjectQueue();
-        q7 = new ObjectQueue();
-        q8 = new ObjectQueue();
     }
 
 
     // Function to get process data from user
-    public void retrieveProcess() {
-        int[] proc_data = new int[3];
+    public void retrieveJobs() {
+        int jobSpec[] = new int[3];
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter number of processes: ");
@@ -47,18 +44,18 @@ public class MultiLevelFeedbackQueue {
         num_proc = input.nextInt();
 
         System.out.println("Arrival time\tPID\tBurst time");
+
         for(int i=0;i<num_proc;i++)
         {
 
-        proc_data[0] = input.nextInt();
-        proc_data[1] = input.nextInt();
-        proc_data[2] = input.nextInt();
+            jobSpec[0] = input.nextInt();
+            jobSpec[1] = input.nextInt();
+            jobSpec[2] = input.nextInt();
 
-        entrance.insert(new Job(proc_data[0], proc_data[1], proc_data[2]));
+            entrance.insert(new Job(jobSpec[0], jobSpec[1], jobSpec[2]));
         }
 
     }
-
 
     //Print a header to display output
     public void outputHeader(){
@@ -78,20 +75,19 @@ public class MultiLevelFeedbackQueue {
             if(!cpu.isOccupied())
                 //(when there is a process in the queues, the highest priority one is to be submitted for exec
                 submitJob();
-            //cpu execution for a cycle
             cpu.process();
+            //cpu execution for a cycle
             ++clock;
-            //if current job is over, it has to be released so CPU state can be marked Free
             if(cpu.currentJob().isFinished()){
+                //if current job is over, it has to be released so CPU state can be marked Free
                 sumTotalTime+=clock-cpu.currentJob().getArrivalTime();
                 departureMessage(cpu.releaseJob());
             }
             //'process is not over but time quantum has run out
             else if(cpu.outOfTime())
-                //give up control of CPU to next allocated process
                 relinquishJob();
         }
-        //CPU is not occupied but one of the queues is not empty
+//CPU is not occupied but one of the queues is not empty
         while(!(!cpu.isOccupied()&&q1.isEmpty()&&q2.isEmpty()&&q3.isEmpty()&&q4.isEmpty())){
             ++clock;
             if(!cpu.isOccupied())
@@ -99,15 +95,16 @@ public class MultiLevelFeedbackQueue {
                 submitJob();
             //execute the process
             cpu.process();
-            //Again if process is over then give control to next process
+
             if(cpu.currentJob().isFinished()){
+                //Again if process is over then give control to next process
                 sumTotalTime+=clock-cpu.currentJob().getArrivalTime();
                 departureMessage(cpu.releaseJob());
             }
-            //If quanta has been exceeded then control is given to next process.
             else if(cpu.outOfTime())
+                //If quanta has been exceeded then control is given to next process.
                 relinquishJob();
-        }        
+        }
     }
 
     //Function to release the job and insert the incomplete job to the next highest priority queue
@@ -115,11 +112,7 @@ public class MultiLevelFeedbackQueue {
         switch (cpu.getQuantum()) {
             case 2 -> q2.insert(cpu.releaseJob());
             case 4 -> q3.insert(cpu.releaseJob());
-            case 8 -> q4.insert(cpu.releaseJob());
-            case 16 -> q5.insert(cpu.releaseJob());
-            case 32 -> q6.insert(cpu.releaseJob());
-            case 64 -> q7.insert(cpu.releaseJob());
-            case 128,256 -> q8.insert(cpu.releaseJob());
+            case 8, 16 -> q4.insert(cpu.releaseJob());
         }
     }
 
@@ -128,15 +121,15 @@ public class MultiLevelFeedbackQueue {
         arrivalMessage((Job)entrance.query());
         totalSize+=((Job)entrance.query()).getSize();
         q1.insert((Job)entrance.extract());
-        ++numberOfProcess;
+        ++numberOfJobs;
     }
 
-    //Function to decide on the next most important thread, using thread complexity
+    //Function to decide on the next most important thread, using thread priority
     private void submitJob(){
         if(!q1.isEmpty()){
             //Assign job for execution, set quantum and quanta to QUANTUMX
             cpu.takeJob((Job)q1.extract());
-            cpu.setQuanta(QUANTUM1);               //copied to qyantum
+            cpu.setQuanta(QUANTUM1);          //copied to qyantum
             sumResponseTime+=clock-cpu.currentJob().getArrivalTime();
         }
         else if(!q2.isEmpty()){
@@ -152,49 +145,24 @@ public class MultiLevelFeedbackQueue {
         else if(!q4.isEmpty()){
             //If queue 3 is empty, insertion happens from queue 4
             cpu.takeJob((Job)q4.extract());
-            cpu.setQuanta(QUANTUM4);            
+            cpu.setQuanta(QUANTUM4);
         }
-
-        else if(!q5.isEmpty()){
-            //If queue 4 is empty, insertion happens from queue 5
-            cpu.takeJob((Job)q5.extract());
-            cpu.setQuanta(QUANTUM5);
-        }
-
-        else if(!q6.isEmpty()){
-            //If queue 5 is empty, insertion happens from queue 6
-            cpu.takeJob((Job)q6.extract());
-            cpu.setQuanta(QUANTUM6);
-        }
-
-        else if(!q7.isEmpty()){
-            //If queue 7 is empty, insertion happens from queue 8
-            cpu.takeJob((Job)q7.extract());
-            cpu.setQuanta(QUANTUM7);
-        }
-
-        else if(!q8.isEmpty()){
-            //If queue 7 is empty, insertion happens from queue 8
-            cpu.takeJob((Job)q8.extract());
-            cpu.setQuanta(QUANTUM8);
-        }
-
     }
 
     //Printing output
     public void outputStats(){
-        System.out.println("Process Completed:" + numberOfProcess);
+        System.out.println("Process Completed:" + numberOfJobs);
         System.out.println("Sum Process Time: %d" + sumTotalTime);
         System.out.println("Total CPU Idle Time: " + (clock-totalSize));
-        System.out.println("Average Response Time: "+((float)sumResponseTime/numberOfProcess));
-        System.out.println("Average Turnarround: " + ((float)sumTotalTime/numberOfProcess));
-        System.out.println("Average Wait Time: " + (((float)sumTotalTime-totalSize)/numberOfProcess));
-        System.out.println("Average Throughput: "+((float)numberOfProcess/sumTotalTime));
+        System.out.println("Average Response Time: "+((float)sumResponseTime/numberOfJobs));
+        System.out.println("Average Turnarround: " + ((float)sumTotalTime/numberOfJobs));
+        System.out.println("Average Wait Time: " + (((float)sumTotalTime-totalSize)/numberOfJobs));
+        System.out.println("Average Throughput: "+((float)numberOfJobs/sumTotalTime));
     }
-
     //Print details when process arrives into execution
     private void arrivalMessage(Job jo){
         System.out.format("Arrival%6d%5d%5d\n",clock,jo.getPid(),jo.getTimeRemaining());
+
     }
 
     //Print details when process exits after completion
@@ -208,6 +176,5 @@ public class MultiLevelFeedbackQueue {
             System.out.format("%10d\n",3);
         else
             System.out.format("%10d\n",4);
-
     }
 }
